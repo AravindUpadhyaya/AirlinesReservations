41fae5c76c71c971e4fa085f72cb4397d6b8d85f
Aravind Upadhyaya <11491@Hexaware.com>
2021-07-03 02:21:03 -0400

    Initial Commit

---
+++ Airlinesreservations/src/main/java/com/hexaware/airlinesreservations/Airlinesreservations/AirlinesreservationsApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.airlinesreservations.Airlinesreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesreservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesreservationsApplication.class, args);
+	}
+
+}
---
+++ Airlinesreservations/src/test/java/com/hexaware/airlinesreservations/Airlinesreservations/AirlinesreservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.airlinesreservations.Airlinesreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesreservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
